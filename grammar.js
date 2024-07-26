d128cfadf06ca1c5aea1a96d7040228f43200e11
Asher Harvey-Smith <asherharveysmith@gmail.com>
2024-07-26 09:59:27 +0100

    more grammar and syntax highlighting

--- grammar.js
+++ grammar.js
@@ -1,15 +1,60 @@
 module.exports = grammar({
-  name: 'APL',
+  name: 'apl',
   rules: {
-    source_file: $ => $.expr,
-    expr:        $ => repeat1(choice(
+    source_file: $ => seq(
+      repeat($.end_stmt),
+      repeat(seq(
+        $.expr,
+        repeat1($.end_stmt),
+      )),
+    ),
+    expr: $ => repeat1(choice(
+      seq(
+        $.lsquare,
+        repeat($.semicolon),
+        repeat(seq(
+          $.expr,
+          repeat1($.semicolon),
+        )),
+        $.rsquare
+      ),
+      seq($.lparen, $.expr, $.rparen),
       $.primitive,
       $.identifier,
       $.character_vector,
-      seq('(', $.expr, ')')
+      $.number,
+      $.defn,
+      $.parameter,
+      $.zilde,
+      $.system,
     )),
-    primitive:        $ => /←|\+|-|×|÷|\*|⍟|⌹|○|!|\?|\||⌈|⌊|⊥|⊤|⊣|⊢|=|≠|≤|<|>|≥|≡|≢|∨|∧|⍲|⍱|↑|↓|⊂|⊃|⊆|⌷|⍋|⍒|⍳|⍸|∊|⍷|∪|∩|~|\/|\\|⌿|⍀|,|⍪|⍴|⌽|⊖|⍉|¨|⍨|⍣|\.|∘|⍤|⍥|@|⍞|⎕|⍠|⌸|⌺|⌶|⍎|⍕/,
+    defn: $ => seq(
+      $.lbrace,
+      repeat($.end_stmt),
+      repeat(seq(
+        $.expr,
+        optional(seq($.colon, $.expr)),
+        repeat1($.end_stmt),
+      )),
+      $.rbrace,
+    ),
+    lparen:           $ => '(',
+    rparen:           $ => ')',
+    lbrace:           $ => '{',
+    rbrace:           $ => '}',
+    lsquare:          $ => '[',
+    rsquare:          $ => ']',
+    colon:            $ => ':',
+    semicolon:        $ => ';',
+    primitive:        $ => /←|\+|-|×|÷|\*|⍟|⌹|○|!|\?|\||⌈|⌊|⊥|⊤|⊣|⊢|=|≠|≤|<|>|≥|≡|≢|∨|∧|⍲|⍱|↑|↓|⊂|⊃|⊆|⌷|⍋|⍒|⍳|⍸|∊|⍷|∪|∩|~|\/|\\|⌿|⍀|,|⍪|⍴|⌽|⊖|⍉|¨|⍨|⍣|\.|∘|⍤|⍥|@|⍠|⌸|⌺|⌶|⍎|⍕/,
     identifier:       $ => /[a-zA-Z_∆][a-zA-Z_∆1-9]*/, // TODO: make correct
-    character_vector: $ => /'([^']|'')*'/,
+    system:           $ => /⍞|⎕|⎕(CT|DCT|DIV|FR|IO|ML|PP|RL|AI|AN|CLEAR|CY|DL|LOAD|OFF|PATH|SAVE|TS|A|D|NULL|C|CMD|CSV|DR|DT|FMT|JSON|MAP|NA|R|S|SH|UCS|USING|VFI|XML|AT|CR|ED|EX|FX|LOCK|MONITOR|OR|NR|PROFILE|REFS|STOP|TRACE|VR|BASE|CLASS|CS|DF|FIX|INSTANCES|NEW|NS|SRC|THIS|ARBIN|ARBOUT|RTL|FAPPEND|FAVAIL|FCHK|FCOPY|FCREATE|FDROP|FERASE|FHIST|FHOLD|FLIB|FNAMES|FNUMS|FPROPS|FRDAC|FRDCI|FREAD|FRENAME|FREPLACE|FRESIZE|FSIZE|FSTAC|FSTIE|FTIE|FUNTIE|MKDIR|NAPPEND|NCOPY|NCREATE|NDELETE|NERASE|NEXISTS|NGET|NINFO|NLOCK|NMOVE|NNAMES|NNUMS|NPARTS|NPUT|NREAD|NRENAME|NREPLACE|NRESIZE|NSIZE|NTIE|NUNTIE|NXLATE|TALLOC|TCNUMS|TID|TKILL|TNAME|TNUMS|TSYNC|TGET|TPOOL|TPUT|TREQ|DM|DMX|EM|EN|EXCEPTION|SIGNAL|TRAP|LC|LX|NC|NL|NSI|RSI|SI|SHADOW|SIZE|STACK|STATE|WA|WSID|XSI|SCV|SVO|SVQ|SVR|SVS|DQ|EXPORT|NQ|WC|WG|WN|WS|WX|Á|AV|AVU|KL|PFKEY|SD|SM|SR|OPT|TC|XT)/i,
+    zilde:            $ => /⍬/,
+    parameter:        $ => /⍺⍺|⍵⍵|⍺|⍵/,
+    inner_quote:      $ => /''/,
+    character_vector: $ => seq("'", repeat(choice($.inner_quote, /[^']/)), "'"),
+    number:           $ => /¯?[0-9]+/,
+    end_stmt:         $ => choice(/⋄|\n/, seq($.comment, '\n')),
+    comment:          $ => /⍝[^\n]*/,
   },
 })
---
+++ queries/highlights.scm
@@ -0,0 +1,19 @@
+(inner_quote)      @constant.character.escape
+(character_vector) @string
+(number)           @constant.numeric
+(zilde)            @constant.builtin
+(comment)          @comment.line
+(system)           @variable.builtin
+(parameter)        @variable.parameter
+(identifier)       @variable
+(end_stmt)         @punctuation.delimiter
+(colon)            @punctuation.delimiter
+(semicolon)        @punctuation.delimiter
+(lparen)           @punctuation.bracket
+(rparen)           @punctuation.bracket
+(lbrace)           @punctuation.bracket
+(rbrace)           @punctuation.bracket
+(lsquare)          @punctuation.bracket
+(rsquare)          @punctuation.bracket
+(primitive)        @function
+
